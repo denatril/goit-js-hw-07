309ce1469e7b48f88e4c29e6e1ea75adcd6cc8a2
oguzhan-ocakoglu <oguzhanocakoglu@gmail.com>
2026-08-05 10:28:55 +0300

    task-5 & stilize

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
